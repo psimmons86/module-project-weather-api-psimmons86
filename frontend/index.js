90c66a058bc47595be70efe492aa4c34ed03362c
Patrick Simmons <hadroncollides@gmail.com>
2024-01-26 14:52:48 -0800

    commit 1

--- frontend/index.js
+++ frontend/index.js
@@ -15,6 +15,24 @@ async function moduleProject4() {
   ]
 
   // 👉 Tasks 1 - 5 go here
+  document.querySelector("#weatherWidget").style.display = 'none';
+
+  document.querySelector('#citySelect').addEventListener('change', async evt => {
+    console.log('selection changed')
+    try {
+      document.querySelector('#citySelect').setAttribute('disabled', 'disabled')
+      document.querySelector('#weatherWidget').style.display = 'none';
+      document.querySelector('.info').textContent = 'Fetching weather data...';
+
+      let city = evt.target.value
+      let url = `http://localhost:3003/api/weather?city=${city}`
+
+      const res = await axios.get(url)
+      
+    } catch(err) {
+      console.log('link busted', err.message)
+    }
+  })
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
